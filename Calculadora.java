ac7d77efb14a972399615b26c2797bb94337f3f8
Paulo Renato (PaulusCorporation) <paulorenato790@gmail.com>
2025-03-18 16:46:31 -0300

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+
+        do {
+            System.out.println("\nEscolha uma operação:");
+            System.out.println("1 - Soma");
+            System.out.println("2 - Subtração");
+            System.out.println("3 - Divisão");
+            System.out.println("4 - Multiplicação");
+            System.out.println("0 - Sair");
+            System.out.print("Opção: ");
+            opcao = scanner.nextInt();
+
+            if (opcao >= 1 && opcao <= 4) {
+                System.out.print("Digite o primeiro número: ");
+                double num1 = scanner.nextDouble();
+                System.out.print("Digite o segundo número: ");
+                double num2 = scanner.nextDouble();
+
+                switch (opcao) {
+                    case 1:
+                        System.out.println("Resultado: " + (num1 + num2));
+                        break;
+                    case 2:
+                        System.out.println("Resultado: " + (num1 - num2));
+                        break;
+                    case 3:
+                        if (num2 != 0) {
+                            System.out.println("Resultado: " + (num1 / num2));
+                        } else {
+                            System.out.println("Erro: divisão por zero!");
+                        }
+                        break;
+                    case 4:
+                        System.out.println("Resultado: " + (num1 * num2));
+                        break;
+                }
+            } else if (opcao != 0) {
+                System.out.println("Opção inválida. Tente novamente.");
+            }
+        } while (opcao != 0);
+
+        System.out.println("Encerrando a calculadora...");
+        scanner.close();
+    }
+}
